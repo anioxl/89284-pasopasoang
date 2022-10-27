5535767663a483ccb24939611456557c21a03c0a
anioxl <104460985+anioxl@users.noreply.github.com>
2022-10-27 16:53:04 -0300

    1 file modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {InicioComponent} from './components/inicio/inicio.component';
-import {ArticulosFamiliasComponent} from './components/articulos-familias/articulos-familias.component';
-import {MenuComponent} from './components/menu/menu.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ArticulosComponent } from './components/articulos/articulos.component';
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from '@angular/router';
 import {APP_BASE_HREF} from '@angular/common';  
@@ -15,9 +16,10 @@ import {APP_BASE_HREF} from '@angular/common';
   imports:      [ BrowserModule, FormsModule, HttpClientModule,RouterModule.forRoot([
     { path: '', redirectTo: '/inicio', pathMatch: 'full' },
     { path: 'inicio', component: InicioComponent },
-    { path: 'articulosfamilias', component: ArticulosFamiliasComponent }
+    { path: 'articulosfamilias', component: ArticulosFamiliasComponent },
+    { path: 'articulos', component: ArticulosComponent }
   ]) ],
-  declarations: [ AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent, MenuComponent ],
+  declarations: [ AppComponent, HelloComponent, InicioComponent, ArticulosFamiliasComponent, MenuComponent, ArticulosComponent ],
   bootstrap:    [ AppComponent ],
   providers: [
     { provide: APP_BASE_HREF, useValue: '/' }]
